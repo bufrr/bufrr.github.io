c72318018ec2e764f6161e5692397d9cc880e220
bufrr <chouyc.adam@gmail.com>
2024-10-02 21:23:03 +0800

    update css

--- build-site.el
+++ build-site.el
@@ -23,13 +23,18 @@
       org-html-head-include-default-style t
       org-html-head "<link rel=\"stylesheet\" href=\"css/style.css\" />")
 
-;; Simple function to get tags
-(defun my-get-filetags (file project)
+;; Function to get creation date
+(defun my-get-creation-date (file)
   (with-temp-buffer
     (insert-file-contents file)
     (goto-char (point-min))
-    (when (re-search-forward "^#\\+FILETAGS:\\s-*\\(.*\\)" nil t)
-      (match-string 1))))
+    (if (re-search-forward "^#\\+CREATED:\\s-*\\(.*\\)" nil t)
+        (match-string 1)
+      "Unknown")))
+
+;; Function to get last modification date
+(defun my-get-modification-date (file)
+  (format-time-string "%Y-%m-%d" (nth 5 (file-attributes file))))
 
 ;; Define the publishing project
 (setq org-publish-project-alist
@@ -43,14 +48,23 @@
          :with-toc t
          :section-numbers nil
          :time-stamp-file nil
-         :html-preamble ,(lambda (info)
-                           (let ((tags (my-get-filetags (plist-get info :input-file)
-                                                        (plist-get info :project))))
-                             (format "<p class=\"date\">Date: %s</p><p class=\"author\">Author: %s</p><p class=\"creator\">%s</p><p class=\"tags\">Tags: %s</p>"
-                                     (format-time-string "%Y-%m-%d" (plist-get info :date))
-                                     (or (plist-get info :author) "")
-                                     (plist-get info :creator)
-                                     (or tags ""))))
+         :html-preamble ""
+         :html-postamble ,(lambda (info)
+                            (let* ((input-file (plist-get info :input-file))
+                                   (creation-date (my-get-creation-date input-file))
+                                   (modification-date (my-get-modification-date input-file))
+                                   (file-name (file-name-nondirectory (plist-get info :output-file)))
+                                   (is-index (string= file-name "index.html")))
+                              (if is-index
+                                  (format "<div id=\"metadata\">%s</div>" (plist-get info :creator))
+                                (format "<div id=\"metadata\">
+Updated: %s<br>
+Created: %s<br>
+%s
+</div>"
+                                        modification-date
+                                        creation-date
+                                        (plist-get info :creator)))))
          :auto-sitemap t
          :sitemap-filename "sitemap.org"
          :sitemap-title "Sitemap"
@@ -59,8 +73,7 @@
                                   (format "[[file:%s][%s]] %s"
                                           file
                                           (org-publish-find-title file project)
-                                          (format-time-string "%Y-%m-%d" (org-publish-find-date file project))))
-         :html-postamble "<p class=\"postamble\">Last Updated %C. Created by %c</p>")))
+                                          (my-get-modification-date file))))))
 
 ;; Generate the site output
 (org-publish-all t)
